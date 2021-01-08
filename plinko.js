b4e25afa5ec3890d94d01eef02a41335ed466d94
smartpramod <76101095+smartpramod@users.noreply.github.com>
2021-01-08 20:49:24 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,30 @@
+class Plinko{
+    constructor(x, y, r){
+
+        var options={
+            isStatic : true,
+            restitution: 1,
+            friction: 0,
+            isStatic:true 
+        }
+
+        this.r=r;
+
+        this.body = Bodies.circle(x, y, this.r, options);
+        World.add(world, this.body);
+    }
+
+    display(){
+
+        var pos = this.body.position;
+        var angle = this.body.angle
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        fill(255);
+        ellipseMode(RADIUS);
+        circle(0, 0, this.r);
+        pop();
+    }
+}
